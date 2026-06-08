ca4ee446f4e7e9d1f24c50a9bb6bfff7dd38608f
Sochigrad <Sochigrad@gmail.com>
2026-06-08 17:18:33 +0300

    Bump Alinexa favicon cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260608-clean-logo-1";
+self.__ALINEXA_SW_VERSION__ = "20260608-favicon-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
