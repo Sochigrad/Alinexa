64d405f5966e48fb13d4928d8dad3a2658eed62a
Sochigrad <Sochigrad@gmail.com>
2026-06-08 13:57:08 +0300

    Bump Alinexa cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260608-plus-column-1";
+self.__ALINEXA_SW_VERSION__ = "20260608-archive-header-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
