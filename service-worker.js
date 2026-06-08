ea97d595c4d6f925f0ca0a83059e8f992b272895
Sochigrad <Sochigrad@gmail.com>
2026-06-08 22:36:08 +0300

    Refresh service worker for column drag v2 build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260608-column-drag-1";
+self.__ALINEXA_SW_VERSION__ = "20260608-column-drag-2";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
