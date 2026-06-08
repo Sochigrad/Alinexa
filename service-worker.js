b13fecdcb52b0bdf642ee97a7acc30ed04055759
Sochigrad <Sochigrad@gmail.com>
2026-06-08 13:13:48 +0300

    Bump service worker for Trello flow

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260526-drag-solid-1";
+self.__ALINEXA_SW_VERSION__ = "20260608-trello-flow-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
