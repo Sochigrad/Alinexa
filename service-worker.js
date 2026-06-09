7bbaacdd6915738e772e3bf04d9cf6c30db17d23
Sochigrad <Sochigrad@gmail.com>
2026-06-09 16:16:43 +0300

    Fix mobile cloud auth session

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260609-mobile-direct-login-1";
+self.__ALINEXA_SW_VERSION__ = "20260609-mobile-cloud-session-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
