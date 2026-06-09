d7acede40df026e50cd4a52f2e7de732ed2a8c66
Sochigrad <Sochigrad@gmail.com>
2026-06-09 13:14:47 +0300

    Refresh service worker for account sheet fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260609-avatar-cabinet-1";
+self.__ALINEXA_SW_VERSION__ = "20260609-auth-sheet-force-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
