1433352ae08048c8c227e968879191f532d87777
Sochigrad <Sochigrad@gmail.com>
2026-06-09 16:11:02 +0300

    Fix mobile direct auth fallback

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260609-auth-timeout-column-clean-1";
+self.__ALINEXA_SW_VERSION__ = "20260609-mobile-direct-login-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
