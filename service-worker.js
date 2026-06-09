d7455220ae7f6c8ed942a48469c6e8212d765d96
Sochigrad <Sochigrad@gmail.com>
2026-06-09 10:50:10 +0300

    Refresh service worker for mobile auth sync

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿self.__ALINEXA_SW_VERSION__ = "20260609-welcome-bg-1";
+﻿self.__ALINEXA_SW_VERSION__ = "20260609-mobile-auth-sync-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
