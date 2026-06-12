93fb93759f131f10d9cf40389972570804bc47b8
Sochigrad <Sochigrad@gmail.com>
2026-06-12 21:46:41 +0300

    Compact board stats and remove tabs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.__ALINEXA_SW_VERSION__ = "20260612-board-redesign-1";
+self.__ALINEXA_SW_VERSION__ = "20260612-compact-board-scroll-1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
